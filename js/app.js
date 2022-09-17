f6ccddbbf0a63face3ceaa672e41766031ed1862
jocelinqueau <queau.j@gmail.com>
2022-09-17 22:20:24 +0200

    init finish

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 
 
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 export default class Sketch {
     constructor(options) {
@@ -14,10 +15,16 @@ export default class Sketch {
         this.camera = new THREE.PerspectiveCamera(70, this.width / this.height, 0.01, 10);
         this.camera.position.z = 1;
 
+
+
         this.renderer = new THREE.WebGLRenderer({ antialias: true });
         this.renderer.setSize(this.width, this.height);
         this.container.appendChild(this.renderer.domElement);
 
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+
+        this.resize()
+        this.setupResize()
         this.addObject()
         this.render()
     }
@@ -31,8 +38,17 @@ export default class Sketch {
         window.requestAnimationFrame(this.render.bind(this))
     }
 
-    resize() {
+    setupResize(){
+       window.addEventListener('resize', this.resize.bind(this))
+    }
 
+
+    resize() {
+        this.width = this.container.offsetWidth;
+        this.height = this.container.offsetHeight;
+        this.renderer.setSize(this.width, this.height);
+        this.camera.aspect = this.width / this.height;
+        this.camera.updateProjectionMatrix()
     }
 
     addObject() {
