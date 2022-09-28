cbc511ef3c739c9b9a1ee6675232e4c118b866e8
jocelinqueau <queau.j@gmail.com>
2022-09-28 17:29:35 +0200

    beginning of scroll

--- js/app.js
+++ js/app.js
@@ -5,6 +5,11 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import fragment from './shaders/fragment.glsl'
 import vertex from './shaders/vertex.glsl'
 
+//
+import imagesLoaded from 'imagesloaded';
+import FontFaceObserver from 'fontfaceobserver';
+//
+
 import ocean from '../img/ocean.jpg'
 
 export default class Sketch {
@@ -19,8 +24,8 @@ export default class Sketch {
 
         this.camera = new THREE.PerspectiveCamera(70, this.width / this.height, 100, 2000);
         this.camera.position.z = 600;
-        console.log('-', Math.atan((this.height / 2) / 600 ) * (180 / Math.PI))
-        this.camera.fov = 2 * Math.atan( (this.height / 2) / 600 ) * (180 / Math.PI)
+        console.log('', Math.atan((this.height / 2) / 600) * (180 / Math.PI))
+        this.camera.fov = 2 * Math.atan((this.height / 2) / 600) * (180 / Math.PI)
 
         this.renderer = new THREE.WebGLRenderer({
             antialias: true,
@@ -32,10 +37,38 @@ export default class Sketch {
 
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 
-        this.resize()
-        this.setupResize()
-        this.addObject()
-        this.render()
+        this.images = [...document.querySelectorAll('img')];
+
+        const fontOpen = new Promise(resolve => {
+            console.log(-1)
+            new FontFaceObserver("Open Sans").load().then(() => {
+                resolve();
+            });
+        });
+
+        const fontPlayfair = new Promise(resolve => {
+            new FontFaceObserver("Playfair Display").load().then(() => {
+                resolve();
+            });
+        });
+
+        // Preload images
+        const preloadImages = new Promise((resolve, reject) => {
+            imagesLoaded(document.querySelectorAll("img"), { background: true }, resolve);
+        });
+
+        let allDone = [fontOpen, fontPlayfair, preloadImages]
+
+        Promise.all(allDone).then(() => {
+            console.log(1)
+            this.addImages()
+            this.setPosition()
+            this.resize()
+            this.setupResize()
+            // this.addObject()
+            this.render()
+        })
+
     }
 
 
@@ -53,8 +86,39 @@ export default class Sketch {
         this.camera.updateProjectionMatrix()
     }
 
+    addImages() {
+        this.imageStore = this.images.map(img => {
+            let { top, left, width, height } = img.getBoundingClientRect();
+            let geometry = new THREE.PlaneGeometry(width, height, 1, 1);
+            let texture = new THREE.TextureLoader().load(img.src);
+            texture.needsUpdate = true;
+            let material = new THREE.MeshBasicMaterial({
+                // color: 0xff0000,
+                map: texture
+            });
+            let mesh = new THREE.Mesh(geometry, material);
+            this.scene.add(mesh)
+            return {
+                img,
+                mesh,
+                top,
+                left,
+                width,
+                height
+            }
+        })
+        console.log(this.imageStore)
+    }
+
+    setPosition() {
+        this.imageStore.forEach(o => {
+            o.mesh.position.y = -o.top + this.height / 2 - o.height / 2;
+            o.mesh.position.x = o.left - this.width / 2 + o.width / 2;
+        })
+    }
+
     addObject() {
-        this.geometry = new THREE.PlaneBufferGeometry(100, 100, 10, 10);
+        this.geometry = new THREE.PlaneGeometry(100, 100, 10, 10);
 
         this.material = new THREE.ShaderMaterial({
             uniforms: {
@@ -74,7 +138,7 @@ export default class Sketch {
     render() {
         this.time += 0.05;
 
-        this.material.uniforms.time.value = this.time;
+        // this.material.uniforms.time.value = this.time;
 
         this.renderer.render(this.scene, this.camera);
         window.requestAnimationFrame(this.render.bind(this))
